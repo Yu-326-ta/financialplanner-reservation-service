46732b73a310ff710878adc4b2b6c8cc9d2c5ea6
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-02-13 14:59:43 +0900

    convert  to ''

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get '/planners/signup', to: 'planners#new'
   get '/clients/signup', to: 'clients#new'
-  root "static_pages#home"
-  get  "/help", to: "static_pages#help"
+  get  '/help', to: 'static_pages#help'
 end
