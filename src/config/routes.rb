cec9bfd781e78ef60f7f400ee2b637788fbcc2cb
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-02-13 13:50:57 +0900

    create planner resource

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/planners/signup', to: 'planners#new'
   get '/clients/signup', to: 'clients#new'
   root "static_pages#home"
   get  "/help", to: "static_pages#help"
