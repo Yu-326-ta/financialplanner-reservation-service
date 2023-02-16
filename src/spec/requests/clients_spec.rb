8737ecfbd7230969217412121fb1a34641273902
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-02-16 13:26:51 +0900

    fix format of spec file

--- src/spec/requests/clients_spec.rb
+++ src/spec/requests/clients_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Clients", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/clients/new"
+RSpec.describe 'Clients', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/clients/new'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
