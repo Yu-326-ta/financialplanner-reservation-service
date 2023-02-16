6f1b5fdd21d2eb5c870e7453be808565773c4276
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-02-16 19:01:12 +0900

    apply validation for client model

--- src/spec/models/client_spec.rb
+++ src/spec/models/client_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:client) { Client.new(name: 'Example Client', email: 'client@example.com') }
+ 
+  it 'clientが有効であること' do
+    expect(client).to be_valid
+  end
+
+  it 'nameが必須であること' do
+    client.name = ''
+    expect(client).to_not be_valid
+  end
+
+  it 'emailが必須であること' do
+    client.email = ''
+    expect(client).to_not be_valid
+  end
+  
+  it 'nameは50文字以内であること' do
+    client.name = 'a' * 51
+    expect(client).to_not be_valid
+  end
+   
+  it 'emailは255文字以内であること' do
+    client.email = "#{'a' * 244}@example.com"
+    expect(client).to_not be_valid
+  end
+
+  it 'emailが有効な形式であること' do
+    valid_addresses = %w[client@exmple.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      client.email = valid_address
+      expect(client).to be_valid
+    end
+  end
+
+  it '無効な形式のemailは失敗すること' do
+    invalid_addresses = %w[client@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      client.email = invalid_address
+      expect(client).to_not be_valid
+    end
+  end
+
+  it 'emailは小文字でDB登録されていること' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    client.email = mixed_case_email
+    client.save
+    expect(client.reload.email).to eq mixed_case_email.downcase
+  end
 end
