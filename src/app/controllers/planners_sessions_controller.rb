fc8a4adf1d1bfd184cf753af795b9d96c32ef82c
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-02-28 12:19:02 +0900

    create planner remember me fuction

--- src/app/controllers/planners_sessions_controller.rb
+++ src/app/controllers/planners_sessions_controller.rb
@@ -1,5 +1,4 @@
 class PlannersSessionsController < ApplicationController
-  
   def new
   end
 
@@ -7,6 +6,7 @@ class PlannersSessionsController < ApplicationController
     planner = Planner.find_by(email: params[:planners_session][:email].downcase)
     if planner && planner.authenticate(params[:planners_session][:password])
       reset_session
+      params[:planners_session][:remember_me] == '1' ? remember(planner) : forget(planner)
       log_in_planner planner
       redirect_to planners_path
     else
@@ -16,7 +16,7 @@ class PlannersSessionsController < ApplicationController
   end
 
   def destroy
-    log_out_planner
+    log_out_planner if logged_in_planner?
     redirect_to root_url, status: :see_other
   end
 end
--- src/app/helpers/planners_sessions_helper.rb
+++ src/app/helpers/planners_sessions_helper.rb
@@ -5,10 +5,23 @@ module PlannersSessionsHelper
     session[:planner_id] = planner.id
   end
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember(planner)
+    planner.remember
+    cookies.permanent.encrypted[:planner_id] = planner.id
+    cookies.permanent[:remember_token] = planner.remember_token
+  end
+
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_planner
-    if session[:planner_id]
-      @current_planner ||= Planner.find_by(id: session[:planner_id])
+    if (planner_id = session[:planner_id])
+      @current_planner ||= Planner.find_by(id: planner_id)
+    elsif (planner_id = cookies.encrypted[:planner_id])
+      planner = Planner.find_by(id: planner_id)
+      if planner && planner.authenticated?(cookies[:remember_token])
+        log_in_planner planner
+        @current_planner = planner
+      end
     end
   end
 
@@ -17,8 +30,16 @@ module PlannersSessionsHelper
     !current_planner.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(planner)
+    planner.forget
+    cookies.delete(:planner_id)
+    cookies.delete(:remember_token)
+  end
+
   # 現在のユーザーをログアウトする
   def log_out_planner
+    forget(current_planner)
     reset_session
     @current_planner = nil
   end
