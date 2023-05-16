3731cc75b2dfcc8f599fbcb4a6e9b7aac5728c55
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-05-16 18:11:48 +0900

    times method change to times_array method

--- src/app/helpers/reservations_helper.rb
+++ src/app/helpers/reservations_helper.rb
@@ -1,21 +1,8 @@
 module ReservationsHelper
-  def times
-    times = ['10:00',
-             '10:30',
-             '11:00',
-             '11:30',
-             '12:00',
-             '12:30',
-             '13:00',
-             '13:30',
-             '14:00',
-             '14:30',
-             '15:00',
-             '15:30',
-             '16:00',
-             '16:30',
-             '17:00',
-             '17:30']
+  def times_array
+    frames_number = 16
+    start_at = '10:00'
+    frames_number.times.map { |frame| (Time.parse(start_at) + 30.minutes * frame).strftime('%H:%M') }
   end
 
   def check_reservation(reservations, day, time)
